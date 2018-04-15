5f60d4c1754458c0996842ca8fab657e74eabed1
Sri Vishnnu Totakura <srivishnu@totakura.in>
2018-04-15 21:09:26 +0200

    Extend Delegate response from +Alexa::Response+
    Gives the advantage of using the same structure for rendering views
    and simplifies the response generation.
    
    Addresses: issue #4

--- lib/alexa/responses/base.rb
+++
@@ -1,9 +0,0 @@
-module Alexa
-  module Responses
-    class Base
-      def as_json(options={})
-        raise "Override #as_json"
-      end
-    end
-  end
-end
--- lib/alexa/responses/delegate.rb
+++ lib/alexa/responses/delegate.rb
@@ -1,17 +1,21 @@
 module Alexa
   module Responses
-    class Delegate < Base
-      def as_json(options={})
-        {
-          "version": "1.0",
-          "response": {
-            "directives": [
-              {
-                "type": "Dialog.Delegate"
-              }
-            ]
+    class Delegate < Alexa::Response
+      def initialize(intent:)
+        @intent = intent
+        @directives = [
+          {
+            "type": "Dialog.Delegate"
           }
-        }
+        ]
+      end
+
+      def partial_path(format: :ssml, filename: nil)
+        return nil
+      end
+
+      def end_session?
+        return false
       end
     end
   end
